484b61aac6aca950e518ff66ff8f5889e8de33f9
MilesChou <jangconan@gmail.com>
2019-08-25 00:57:31 +0800

    Add move zero to backward PHP version

--- scripts/php-test.sh
+++ scripts/php-test.sh
@@ -8,4 +8,5 @@ if [ -e ./scripts/php-test.sh ]; then
 fi
 
 php longest_substring.php
+php move_zero_to_backward.php
 php sort_fixed_number.php
---
+++ src/php/move_zero_to_backward.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * NOTICE: Ensure PHP version is 7.1+
+ */
+
+/**
+ * @param array $input
+ * @return array
+ */
+function solution(array $input)
+{
+    $lastZero = 0;
+
+    foreach ($input as $k => &$v) {
+        if (0 !== $v) {
+            [$input[$lastZero], $v] = [$v, $input[$lastZero]];
+            $lastZero++;
+        }
+    }
+    return $input;
+}
+
+// Example test
+assert([1, 3, 12, 0, 0] === solution([0, 1, 0, 3, 12]));
+assert([2, 1, 3, 4, 0, 0, 0, 0, 0, 0] === solution([0, 0, 0, 2, 0, 1, 3, 4, 0, 0]));
+
+// Empty list
+assert([] === solution([]));
+
+// One number test
+assert([0] === solution([0]));
+assert([1] === solution([1]));
+
+// No zero / non-zero
+assert([0, 0, 0] === solution([0, 0, 0]));
+assert([1, 2, 3] === solution([1, 2, 3]));
+assert([1, 2, 3, 4] === solution([1, 2, 3, 4]));
