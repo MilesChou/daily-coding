86c030a9a2725ecebcb2b890b49ad88dfbe952f5
MilesChou <jangconan@gmail.com>
2019-08-25 00:35:09 +0800

    Add kotlin test

--- scripts/kotlin-test.sh
+++ scripts/kotlin-test.sh
@@ -11,10 +11,11 @@ FILES="
 move_zero_to_backward.kts
 "
 
-if [ "`uname`" = "Darwin" ]; then
-    for file in ${FILES}; do
-      docker run --rm -it -v `pwd`:/source zenika/kotlin:1.3-alpine kotlinc -script /source/${file}
-    done
-else
-    kotlinc -script ${file}
-fi
+for file in ${FILES}; do
+    if [ "`uname`" = "Darwin" ]; then
+        docker run --rm -it -v `pwd`:/source zenika/kotlin:1.3-alpine kotlinc -script /source/${file}
+    else
+        kotlinc -script ${file}
+    fi
+done
+
